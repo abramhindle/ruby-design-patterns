00d36de6f726e0cd826fe426552078a10e25a2f1
Abram Hindle <abram.hindle@ualberta.ca>
2013-02-19 19:37:49 -0700

    State-Pattern example

--- FactoryMethod.rb
+++ FactoryMethod.rb
@@ -1,30 +1,50 @@
 class Printer
   def print_header(header)
+    puts(header)
   end
   def print_body(body)
+    puts(body)
   end
   def print_footer(footer)
+    puts(footer)
   end
 end
 class AsciiPrinter < Printer
 end
 class AsciiDraftPrinter < AsciiPrinter
+  def print_header(header)
+  end
+  def print_footer(header)
+  end
 end
 class PostScriptPrinter < Printer
 end
 class PostScriptDraftPrinter < PostScriptPrinter
+  def print_header(header)
+  end
+  def print_footer(header)
+  end
 end
 
 
 class Report
+  def header()
+    "header"
+  end
+  def body()
+    "body"
+  end
+  def footer()
+    "footer"
+  end
   def get_printer(type)
     throw "Abstract: Please Overload"
   end
   def print_report(type)
     printer = get_printer(type)
-    printer.print_header(@header)
-    printer.print_body(@body)
-    printer.print_footer(@footer)
+    printer.print_header(header)
+    printer.print_body(body)
+    printer.print_footer(footer)
   end
 end
 class DraftReport < Report
@@ -45,3 +65,12 @@ class BasicReport < Report
     end
   end
 end
+
+puts("Draft")
+d = DraftReport.new()
+d.print_report("ascii")
+d.print_report("postscript")
+puts("Basic")
+b = BasicReport.new()
+b.print_report("ascii")
+b.print_report("postscript")
